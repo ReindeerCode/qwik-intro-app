dffb89d82be15451e872ff56a154d0c937c01d33
MJRudolph <ReindeerCode@gmail.com>
2023-08-21 18:54:34 -0400

    testing creating a new component$ inside the same page as another component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
-  return <div>Llamas Rule!</div>;
+  return <>
+<Test/>
+  <div>Llamas Rule!</div></>;
 });
+
+
+export const Test = component$(() => {
+  return <h1>Here is a New H1 Tag</h1>
+})
