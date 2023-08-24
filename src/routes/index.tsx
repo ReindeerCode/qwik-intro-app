207b8c4a37120e7541eaeb9cc009e5307149e053
MJRudolph <ReindeerCode@gmail.com>
2023-08-24 19:27:14 -0400

    creating first button, on click console log message

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,18 @@
 import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
-  return <>
-<Test/>
-  <div>Llamas Rule!</div></>;
+  return (
+    <>
+      <Test />
+      <div>Llamas Rule!</div>
+    </>
+  );
 });
 
-
 export const Test = component$(() => {
-  return <h1>Here is a New H1 Tag</h1>
-})
+  return <h1>Here is a New H1 Tag</h1>;
+});
+
+export const HelloButton = component$(() => {
+  return <button onClick$={() => console.log("Hello")}>Click Me, Now!</button>;
+});
