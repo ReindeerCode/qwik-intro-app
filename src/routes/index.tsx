169a5e3423bab84704a7ec95804de0c8b13a9a0d
MJRudolph <ReindeerCode@gmail.com>
2023-08-28 20:19:48 -0400

    adding Slot to do content projection

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,12 @@ import { Slot, component$, useSignal } from "@builder.io/qwik";
 
 export default component$(() => {
 
-const isTestText1Visable = useSignal(false)
+const isTestText1VisableSignal = useSignal(false)
 
   return (
     <>
-            <button onClick$={() => isTestText1Visable.value = !isTestText1Visable.value }>Button Text</button>
-      {isTestText1Visable.value ? <Test /> :null}
+            <button onClick$={() => isTestText1VisableSignal.value = !isTestText1VisableSignal.value }>Button Text</button>
+      {isTestText1VisableSignal.value ? <Test>This is an example of using QWIK's "Slot" component to do content projection.</Test> :null}
     </>
   );
 });
@@ -16,7 +16,7 @@ export const Test = component$(() => {
   return (
     <>
       <h3>Here is the test text</h3>
-      
+      <Slot/>
     </>
   );
 });
