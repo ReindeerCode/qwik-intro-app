5ff89784a4b2f46f5c50ceb1b056fc785e638a22
MJRudolph <ReindeerCode@gmail.com>
2023-09-04 15:25:12 -0400

    aligning header index code to tutorial because I keep getting errors that I cannot explain

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,93 @@
 import { Slot, component$, useSignal } from "@builder.io/qwik";
 
 export default component$(() => {
-  const isTestText1VisableSignal = useSignal(false);
+  const isMiskoVisibleSignal = useSignal(false);
 
   return (
     <>
       <button
         onClick$={() =>
-          (isTestText1VisableSignal.value = !isTestText1VisableSignal.value)
+          (isMiskoVisibleSignal.value = !isMiskoVisibleSignal.value)
         }
       >
         Button Text
       </button>
-      {isTestText1VisableSignal.value ? (
-        <Test>
+      {isMiskoVisibleSignal.value ? (
+        <Misko>
           <h3>
             This is an example of using QWIK's "Slot" component to do content
-            projection. And, will appear on bottom. 
+            projection. And, will appear on bottom.
           </h3>
-        </Test>
+        </Misko>
       ) : null}
     </>
   );
 });
 
-export const Test = component$(() => {
+export const Misko = component$(() => {
   return (
     <>
       <h3>
-        This is the text contained in the Test component, and will appear on top because I placed the Slot tag below this text in the editor.
+        This is the text contained in the Test component, and will appear on top
+        because I placed the Slot tag below this text in the editor.
       </h3>
       <Slot />
     </>
   );
 });
+
+// import { Signal, Slot, component$, useSignal } from "@builder.io/qwik";
+
+// export default component$(() => {
+//   const isTestText1VisableSignal = useSignal(false);
+//   const didTestTextGetSeenSignal = useSignal(false);
+
+//   return (
+//     <>
+//       <button
+//         onClick$={() =>
+//           (isTestText1VisableSignal.value = !isTestText1VisableSignal.value)
+//         }
+//       >
+//         Button Text
+//       </button>
+
+//       <TestTextSeer gotTestTextSignal={didTestTextGetSeenSignal}/>
+
+//       {isTestText1VisableSignal.value ? (
+//         <Test>
+//           <h3>
+//             This is an example of using QWIK's "Slot" component to do content
+//             projection. And, will appear on bottom.
+//           </h3>
+//         </Test>
+//       ) : null}
+//     </>
+//   );
+// });
+
+// interface TestTextSeerProps{
+//   gotTestTextSignal: Signal<boolean>
+// }
+
+// export const TestTextSeer = component$((props: TestTextSeerProps ) => {
+//   return (
+//     <>
+//       <button onClick$={() =>{
+//         props.gotTestTextSignal.value = true
+//       }}>
+//         Click Me To See Text Text
+//         </button>
+//     </>
+//   );
+
+// export const Test = component$(() => {
+//   return (
+//     <>
+//       <h3>
+//         This is the text contained in the Test component, and will appear on top because I placed the Slot tag below this text in the editor.
+//       </h3>
+//       <Slot />
+//     </>
+//   );
+// });
