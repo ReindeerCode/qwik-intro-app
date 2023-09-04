83f27833a2bff184653ec2cb6e59a13ccbb6389a
MJRudolph <ReindeerCode@gmail.com>
2023-09-04 15:50:16 -0400

    added useTask code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,59 @@
-import { Slot, component$, useSignal } from "@builder.io/qwik";
+import {
+  Signal,
+  Slot,
+  component$,
+  useSignal,
+  useTask$,
+} from "@builder.io/qwik";
 
 export default component$(() => {
   const isMiskoVisibleSignal = useSignal(false);
+  const didHeGetABeerSignal = useSignal(false);
 
+  useTask$(({ track }) => {
+    track(() => didHeGetABeerSignal.value);
+
+    if (didHeGetABeerSignal.value) {
+      isMiskoVisibleSignal.value = true;
+    }
+  });
   return (
     <>
-      <button
+      {/* <button
         onClick$={() =>
           (isMiskoVisibleSignal.value = !isMiskoVisibleSignal.value)
         }
       >
         Button Text
-      </button>
+      </button> */}
+
+      <BeerGiver gotBeerSignal={didHeGetABeerSignal} />
       {isMiskoVisibleSignal.value ? (
         <Misko>
-          <h3>
+          <h5>
             This is an example of using QWIK's "Slot" component to do content
             projection. And, will appear on bottom.
-          </h3>
+          </h5>
         </Misko>
       ) : null}
+    </>
+  );
+});
+
+interface BeerGiverProps {
+  gotBeerSignal: Signal<boolean>;
+}
+
+export const BeerGiver = component$((props: BeerGiverProps) => {
+  return (
+    <>
+      <button
+        onClick$={() => {
+          props.gotBeerSignal.value = true;
+        }}
+      >
+        Give Misko A Beer
+      </button>
     </>
   );
 });
@@ -27,10 +61,10 @@ export default component$(() => {
 export const Misko = component$(() => {
   return (
     <>
-      <h3>
+      <h5>
         This is the text contained in the Test component, and will appear on top
         because I placed the Slot tag below this text in the editor.
-      </h3>
+      </h5>
       <Slot />
     </>
   );
